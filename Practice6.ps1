a7433757ea06d9b4f1dcd9293fbcb9270849c031
RaikoBel <raiko.belausev@voco.ee>
2023-11-22 15:30:05 +0200

    Practice6 solution

---
+++ Practice6.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Path "C:\Users\raiko\Desktop\test.txt"
+$file = Get-ChildItem -Path "C:\Users\raiko\Desktop\test.txt" | where {$_.Name -like "*.csv"} | Select Name.Lenght
+$sizeinKB = $file.Lenght/1KB
+$sizeinMB = $file.Lenght/1MB
+
+Write-Host "nFileName : "$file.Name
+Write-Host "Size in KB :"$sizeinKB
+Write-Host "Sine in MB: "$sizeinMB
