2764aaab62afb84db8f76c788f36bfc65b54ed4a
RaikoBel <raiko.belausev@voco.ee>
2023-11-22 15:29:05 +0200

    Practice4 solution

---
+++ practice4.ps1
@@ -0,0 +1,36 @@
+﻿$value1 = Read-Host "Enter the first value:"
+$value2 = Read-Host "Enter the second value:"
+
+if($value1 -gt $value2)
+{
+    Write-Host "The higer number is: $value1"
+}
+else
+{
+    Write-Host "The higer number is: $value2"
+}
+
+##########
+Write-Host "Select a country" -ForegroundColor Yellow
+Write-Host "1: India" -ForegroundColor Cyan
+Write-Host "2: Australia" -ForegroundColor Cyan
+Write-Host "3: China" -ForegroundColor Cyan
+
+$valik = Read-Host "Please select country"
+
+if($valik -eq "1")
+{
+    Write-Host "India's capital is Delhi" -ForegroundColor Green
+}
+elseif($valik -eq "2")
+{
+    Write-Host "Australia's capital is Canberra" -ForegroundColor Green
+}
+elseif($valik -eq "3")
+{
+    Write-Host "China's capital is Bejing" -ForegroundColor Green
+}
+else
+{
+    Write-Host "Wrong choise" -ForegroundColor Red
+}
