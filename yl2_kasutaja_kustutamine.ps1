48714de3ca0cef14a9ae5918992c296987920cf9
RaikoBel <raiko.belausev@voco.ee>
2023-12-07 11:40:08 +0200

    Kasutada kustutamise skript

---
+++ yl2_kasutaja_kustutamine.ps1
@@ -0,0 +1,23 @@
+﻿# Küsin kasutajalt eesnime
+$eesnimi = Read-Host "Sisesta kasutaja eesnimi"
+
+# Küsin kasutajalt perekonnanime
+$perekonnanimi = Read-Host "Sisesta kasutaja perekonnanimi"
+
+# Loome kasutajanime
+$kasutajanimi = "$($eesnimi.ToLower()).$($perekonnanimi.ToLower())"
+
+# Proovin kustutada kasutaja
+try {
+    Remove-LocalUser -Name $kasutajanimi -ErrorAction Stop
+    Write-Host "Kasutaja '$kasutajanimi' kustutatud edukalt!" -ForegroundColor Green
+}
+catch {
+    $errorMessage = $_.Exception.Message
+    if ($errorMessage -like '*not found*') {
+        Write-Host "Kasutajat '$kasutajanimi' ei leitud!" -ForegroundColor Yellow
+    }
+    else {
+        Write-Host "Kasutaja kustutamine ebaõnnestus! Veateade: $errorMessage" -ForegroundColor Red
+    }
+}
